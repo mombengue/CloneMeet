9435f5822c604365eca4bb5bc0d99973b67d7fdc
mombengue <doumomb2010@gmail.com>
2022-09-19 14:05:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# 🛍 Ecommerce Tailwind CSS / Alpine JS  
+
+> Home
+
+![home](https://user-images.githubusercontent.com/31158553/190677227-3a7e0bdd-dd6a-464f-ac1f-4cee67e8a832.png)
+
+> Product
+
+![product](https://user-images.githubusercontent.com/31158553/190677363-706d9470-bcfd-4e1d-b52d-4ad379f23904.png)
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const  defaultTheme = require ('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extends: {
+      fontFamily : { 
+        sans : ['Inter' , ...defaultTheme.fontFamily.sans] , 
+      } , 
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
